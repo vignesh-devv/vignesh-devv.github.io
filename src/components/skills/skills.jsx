30e6f09229cffeb0f2a80d2e81003cf75c70765a
vignesh-devv <118846334+vignesh-devv@users.noreply.github.com>
2022-11-22 13:27:30 -0600

    init

---
+++ src/components/skills/skills.jsx
@@ -0,0 +1,100 @@
+import React from 'react';
+import './skills.css';
+import C from '../../assets/skills/c++.svg';
+import Py from '../../assets/skills/py.svg';
+import JS from '../../assets/skills/js1.svg';
+import Dart from '../../assets/skills/dart.svg';
+import Php from '../../assets/skills/php.svg';
+import Flutter from '../../assets/skills/flutter.svg';
+import OpenCv from '../../assets/skills/opencv.svg';
+import Reactjs from '../../assets/skills/react.svg';
+import HtmlLogo from '../../assets/skills/html.svg';
+import CssLogo from '../../assets/skills/css.svg';
+import FlaskLogo from '../../assets/skills/flask.svg';
+import Heroku from '../../assets/skills/heroku.svg';
+import Git from '../../assets/skills/git.svg';
+import BitBucket from '../../assets/skills/bitbucket.svg';
+import Mongo from '../../assets/skills/mongodb.svg';
+import Mysql from '../../assets/skills/sql.svg';
+import GraphQl from '../../assets/skills/graphql.svg';
+import Hasura from '../../assets/skills/hasura.svg';
+import Firebase from '../../assets/skills/firebase.svg';
+import D1 from '../../assets/skills/d1.svg';
+import D2 from '../../assets/skills/d2.svg';
+import D3 from '../../assets/skills/d3.svg';
+import D4 from '../../assets/skills/d4.svg';
+import D5 from '../../assets/skills/d5.svg';
+import D6 from '../../assets/skills/d6.svg';
+import D7 from '../../assets/skills/d7.svg';
+
+
+/*
+Programming Languages: Python, C/C++, Dart, HTML/CSS, JavaScript, PHP, XML.
+Tools, Frameworks and Databases: Flutter, Open-CV, Flask, Git, Bit-bucket, Heroku, MongoDB, MySQL, GraphQL, Hasura, Firebase.  
+*/
+const Skills = () => {
+  return (
+    <section className="services container section" id="skills">
+      <h2 className='section__title'>My Skills</h2>
+
+      <div className='services__container grid'>
+        <h3 className='skills_desc'>I like to take responsibility to craft aesthetic and accessible user experience using state of the art framework.
+        </h3>
+
+        
+
+        <h2 className='skills_head'> PROGRAMMING LANGUAGES
+        </h2>
+        <div className='skillsContainer'>
+          <div className='skills'>
+            <img src={C} alt="" className='skills_img' />
+            <img src={Py} alt="" className='skills_img' />
+            <img src={JS} alt="" className='skills_img' />
+            <img src={Dart} alt="" className='skills_img' />
+            <img src={Php} alt="" className='skills_img' />
+            <img src={HtmlLogo} alt="" className='skills_img' />
+          <img src={CssLogo} alt="" className='skills_img' />
+          </div>  
+        </div>
+        
+        <h2 className='skills_head'> DEV TOOLS & FRAMEWORKS
+        </h2>
+        <div className='skillsContainer'>
+          <div className='skills'>
+          <img src={Flutter} alt="" className='skills_img' />
+          <img src={Reactjs} alt="" className='skills_img' />
+          <img src={OpenCv} alt="" className='skills_img' />
+          <img src={FlaskLogo} alt="" className='skills_img' />
+          <img src={Heroku} alt="" className='skills_img' />
+          <img src={Git} alt="" className='skills_img' />
+          <img src={BitBucket} alt="" className='skills_img' />
+          <img src={Mongo} alt="" className='skills_img' />
+          <img src={Mysql} alt="" className='skills_img' />
+          <img src={Firebase} alt="" className='skills_img' />
+          <img src={GraphQl} alt="" className='skills_img' />
+          <img src={Hasura} alt="" className='skills_img' />
+          
+        </div>  
+      </div>
+
+      <h2 className='skills_head'> USER EXPERIENCE DESIGN TOOLS
+        </h2>
+        <div className='skillsContainer'>
+          <div className='skills'>
+            <img src={D4} alt="" className='skills_img' />
+            <img src={D2} alt="" className='skills_img' />
+            <img src={D1} alt="" className='skills_img' />
+            <img src={D7} alt="" className='skills_img' />
+            {/* <img src={D5} alt="" className='skills_img' /> */}
+            <img src={D3} alt="" className='skills_img' />
+            <img src={D6} alt="" className='skills_img' />
+          </div>  
+        </div>
+        
+      </div>
+      
+    </section>
+  )
+}
+
+export default Skills
---
+++ src/components/work/workData.jsx
@@ -0,0 +1,168 @@
+
+
+const WorkData = [
+    /* Web Dev */
+    {        
+        title: "Codar",
+        desc: "CODAR is a framework built using PyTorch to analyze post (Text+Media) and predict cyber bullying and offensive content.",
+        category: "Web Dev",
+        number: "01.",
+        stack: "Html5 • CSS • JS • Flask • PyTorch • Bert" ,
+        url: "https://github.com/axenhammer/CODAR"   
+    },
+
+    {        
+        title: "Forese Contact Book",
+        desc: "Contact Book is a web based software to create, store and manage HR contacts for the annual placement fest. Built as part of the placement cell.",
+        category: "Web Dev",
+        number: "02.",
+        stack: "Html5 • CSS • JS • MySql • PHP" ,
+        url: "https://github.com/Vignesh0404/FORESE-CMS"    
+    },
+
+    {         
+        title: "Bhumi creatives",
+        desc: "Bhumi Creatives is an open source web application for NGOs to upload designs, contents for other organizations to reuse. Built for Bhumi NGO. ",
+        category: "Web Dev",
+        number: "03.",
+        stack: "Html5 • CSS • JS • MySql • PHP" ,
+        url: "https://github.com/tech-for-change/Bhumi-Creatives"    
+    },
+
+    {         
+        title: "Bhumi Donation",
+        desc: "A Funding platform that facilitates donations to underprivileged students, letting donors choose which children to donate directly to.",
+        category: "Web Dev",
+        number: "04.",
+        stack: "Html5 • CSS • JS • MySql • PHP" ,
+        url: "https://github.com/bearlike/Donation-Web Devsite"    
+    },
+
+    {
+        title: "Resume Scanner",
+        desc: "Resume Scanner is to analyse the Resume of the student and comparing it with the company's requirements. Buit as part of the placement cell.",
+        category: "Web Dev",
+        number: "05.",
+        stack: "Html5 • CSS • JS • NLP • RAKE" ,
+        url: "https://github.com/Vignesh0404/Resume-Scanner"    
+    },
+
+    {
+        title: "Portfolio",
+        desc: "This portfolio website to tell a bit about myself, my interests, showcase my core experiences and the projects that I had the opportunity to work on.",
+        category: "Web Dev",
+        number: "06.",
+        stack: "React JS" ,
+        url: "https://github.com/Vignesh0404/vignesh0404.github.io"    
+    },
+
+
+    /* Mobile Dev */
+    {
+        title: "Emotion Manipulator",
+        desc: "A Mobile application which manipulates 5 different emotions of a person using a single input image. Built as part of the undergrad final year project",
+        category: "Mobile Dev",
+        number: "01.",
+        stack: "Java • Open Cv • Flask • Firebase" ,
+        url: "https://github.com/Vignesh0404/Emotion-Manipulator"   
+    }, 
+
+    {
+        title: "Monument Universe",
+        desc: "Monument Universe is an intuitively designed App that fills in as a virtual tour guide to historically rich monuments in South India.",
+        category: "Mobile Dev",
+        number: "02.",
+        stack: "Flutter • Graph QL • Hasura • Bloc" ,
+        url: "https://github.com/Vignesh0404/Monuments-app"   
+    },
+
+    {
+        title: "Quiz Software App",
+        desc: "An simple mobile app built in collabration with my undergrad university for the students to test their technical/aptitude skills.",
+        category: "Mobile Dev",
+        number: "03.",
+        stack: "Dart • Firebase • MySQL" ,
+        url: "https://github.com/Vignesh0404/Quiz-Software-App"    
+    },    
+
+    {
+        title: "Flutter UI Kit",
+        desc: "As part of the open source development, took up a 30 day app ui kit challange to develop 30 applications which will be useful for fellow devs.",
+        category: "Mobile Dev",
+        number: "04.",
+        stack: "Flutter • Dribble" ,
+        url: "https://github.com/Vignesh0404/Flutter-UI-Kit"    
+    },
+
+    {
+        title: "Hospital Management",
+        desc: "A Mobile application for the hospitals and doctors to store and track of patient data, admission and discharge details.",
+        category: "Mobile Dev",
+        number: "05.",
+        stack: "Flutter • MongoDB • Firebase • Flask" ,
+        url: "https://github.com/Vignesh0404/Hospital-Referral-Management-Sys"    
+    },
+
+    {
+        title: "Loan Manager",
+        desc: "Loan Manager is a simple, scalable Mobile application built using Flutter which is used to book an appointment for your loans at your doorstep.",
+        category: "Mobile Dev",
+        number: "06.",
+        stack: "Flutter • Google Maps API" ,
+        url: "https://github.com/Vignesh0404/NFN-LABS-LoanManager"    
+    },
+
+
+    /* UX/UI */
+
+    {
+        title: "Deliver IT",
+        desc: "Deliver IT is a mobile application that serves as a platform that lets users send and receive local deliveries on the same day.",
+        category: "UX/UI",
+        number: "01.",
+        stack: "Mobile • Figma" ,
+        url: "https://www.figma.com/proto/PbUG1k2gdhFwmjTvjvzVLa/Deliver-IT?page-id=0%3A1&node-id=1%3A2&viewport=512%2C5468%2C0.26&scaling=scale-down-width"    
+    },
+
+    {
+        title: "RMS Scales",
+        desc: "As part of my Research Internship under Dr Ragavendara Shetty's tutelage, designed and developed his thesis on dental anxiety.",
+        category: "UX/UI",
+        number: "02.",
+        stack: "Mobile • Figma" ,
+        url: "https://www.figma.com/proto/OBlcS9BuOMtVTXeTC9e8kR/RMS-Scales?page-id=0%3A1&node-id=1%3A2&viewport=526%2C365%2C0.02&scaling=scale-down-width"    
+    },
+
+    
+
+    {
+        title: "25 Days of Design",
+        desc: "To pursue my yearn to explore the field of UX/UI, I took up a sprint to design projects every day for 25 days. This is the compilation of the results.",
+        category: "UX/UI",
+        number: "03.",
+        stack: "Mobile • Figma" ,
+        url: "https://www.figma.com/proto/gTyNJq32oKmttK9jiJeOZQ/25-Days-of-Design?page-id=0%3A1&node-id=1%3A2&viewport=533%2C938%2C0.07&scaling=min-zoom"    
+    },
+
+    {
+        title: "Dine Out",
+        desc: "Dine Out is a mobile-based application that aims to get users discounts every single time you go out to our partnered restaurants.",
+        category: "UX/UI",
+        number: "04.",
+        stack: "Mobile • Adobe xd" ,
+        url: "https://www.figma.com/proto/gkwUhpXKTthGtdZXwrCZcg/Dine-Out?page-id=0%3A1&node-id=1%3A2&viewport=526%2C365%2C0.02&scaling=scale-down-width"    
+    },
+
+    {
+        title: "Quiz Application",
+        desc: "An simple mobile app built in collabration with my undergrad university for the students to test their technical/aptitude skills.",
+        category: "UX/UI",
+        number: "05.",
+        stack: "Mobile • Sketch" ,
+        url: "https://www.figma.com/proto/VnR6IjipVPEhkAG7mjBzbz/Quiz?page-id=0%3A1&node-id=1%3A3&viewport=526%2C365%2C0.02&scaling=scale-down-width"    
+    },
+
+
+];
+
+export default WorkData
